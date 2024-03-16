794a71e1a6262d97fdbfdd58a83d38b223e5a678
dimsav <savin77@gmail.com>
2024-03-16 23:16:35 +0300

    fix

--- start_ubuntu.sh
+++ start_ubuntu.sh
@@ -1,5 +1,6 @@
 echo start mqttbkp_srv...
-source venv/bin/activate && nohup python3 ./mqttbkp_srv.py >mqttbkp_srv_log.txt 2>&1 &
+
+source ./venv/bin/activate && nohup python3 ./mqttbkp_srv.py >mqttbkp_srv_log.txt 2>&1 &
 echo started ok
 
 sleep 3
@@ -10,7 +11,8 @@ ps | grep python
 
 sleep 2
 echo start rest_srv...
-source venv/bin/activate && nohup python3 ./rest_srv.py >rest_srv_log.txt 2>&1 &
+
+source ./venv/bin/activate && nohup python3 ./rest_srv.py >rest_srv_log.txt 2>&1 &
 echo started ok
 
 sleep 3
@@ -19,5 +21,3 @@ echo servers started with ps:
 ps | grep python
 
 echo TODO: change ps grepping to pgrep
-
-
